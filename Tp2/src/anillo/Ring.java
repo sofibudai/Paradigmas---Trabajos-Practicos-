328f213858b95bcb5957bd27357eb76bc90e6dfc
Sofia Budai <sbudai@udesa.edu.ar>
2024-04-09 20:29:54 -0300

    Cambios Sofi clase Teorica

---
+++ Tp2/src/anillo/Ring.java
@@ -0,0 +1,64 @@
+package anillo;
+
+public class Ring {
+    private Node current;
+
+    private static class Node {
+        Object elem;
+        Node next;
+
+        Node(Object elem) {
+            this.elem = elem;
+        }
+    }
+
+    public Ring next() {
+        if (this.current != null) {
+            this.current = this.current.next;
+            return this;
+        }
+        throw new RuntimeException();
+    }
+
+    public Object current() {
+        if (this.current != null) {
+            return this.current.elem;
+        }
+        throw new RuntimeException();
+    }
+
+    public Ring add(Object cargo) {
+        Node newNode = new Node(cargo);
+        if (this.current == null) {
+            newNode.next = newNode;
+            this.current = newNode;
+
+        } else {
+            newNode.next = this.current.next;
+            this.current.next = newNode;
+            this.current = newNode;
+        }
+        //this.current = newNode;
+        return this;
+    }
+
+    public Ring remove() {
+        if (this.current == null) {
+            throw new RuntimeException();
+        }
+        if (this.current.next == this.current) {
+            this.current = null;
+        } else {
+            Node nodeToRemove = this.current;
+            this.current = nodeToRemove.next;
+            Node previousNode = this.current;
+            while (previousNode.next != nodeToRemove) {
+                previousNode = previousNode.next;
+            }
+            previousNode.next = this.current;
+        }
+        return this;
+    }
+}
+
+
